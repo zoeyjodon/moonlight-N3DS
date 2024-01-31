32389be078bfd107400ff4bd3b86412cf344e274
zoey jodon <jodonlucas@gmail.com>
2024-01-31 16:34:51 -0500

    Clean up mvd decode driver Split YUV and RGB buffers Move YUV processing to the end, since it seems to be our longest process Explicitly enable the YUV conversion end interrupt

--- src/video/n3ds_video_mvd.c
+++ src/video/n3ds_video_mvd.c
@@ -33,12 +33,14 @@
 #define N3DS_DEC_BUFF_SIZE 23
 
 // General decoder and renderer state
-static void* n3ds_buffer;
-static size_t n3ds_buffer_size;
+static void* nal_unit_buffer;
+static size_t nal_unit_buffer_size;
 static MVDSTD_Config mvdstd_config;
+Handle conversion_finish_event_handle = NULL;
 
 static int image_width, image_height, surface_width, surface_height, pixel_size;
-static u8* img_buffer;
+static u8* yuv_img_buffer;
+static u8* rgb_img_buffer;
 static bool first_frame = true;
 
 static int n3ds_init(int videoFormat, int width, int height, int redrawRate, void* context, int drFlags) {
@@ -68,6 +70,11 @@ static int n3ds_init(int videoFormat, int width, int height, int redrawRate, voi
     fprintf(stderr, "Failed to set Y2RU params\n");
     return -1;
   }
+  status = Y2RU_SetTransferEndInterrupt(true);
+  if (status) {
+    fprintf(stderr, "Failed to enable Y2RU interrupt\n");
+    return -1;
+  }
 
   surface_height = 240;
   if (width > 400) {
@@ -83,14 +90,19 @@ static int n3ds_init(int videoFormat, int width, int height, int redrawRate, voi
   image_width = width;
   image_height = height;
   pixel_size = gspGetBytesPerPixel(px_fmt);
-  img_buffer = linearMemAlign(width * height * pixel_size, 0x80);
-  if (!img_buffer) {
+  yuv_img_buffer = linearMemAlign(width * height * pixel_size, 0x80);
+  if (!yuv_img_buffer) {
+    fprintf(stderr, "Out of memory!\n");
+    return -1;
+  }
+  rgb_img_buffer = linearMemAlign(width * height * pixel_size, 0x80);
+  if (!rgb_img_buffer) {
     fprintf(stderr, "Out of memory!\n");
     return -1;
   }
 
-  ensure_linear_buf_size(&n3ds_buffer, &n3ds_buffer_size, INITIAL_DECODER_BUFFER_SIZE + AV_INPUT_BUFFER_PADDING_SIZE);
-  mvdstdGenerateDefaultConfig(&mvdstd_config, image_width, image_height, surface_width, surface_height, NULL, img_buffer, NULL);
+  ensure_linear_buf_size(&nal_unit_buffer, &nal_unit_buffer_size, INITIAL_DECODER_BUFFER_SIZE + AV_INPUT_BUFFER_PADDING_SIZE);
+  mvdstdGenerateDefaultConfig(&mvdstd_config, image_width, image_height, surface_width, surface_height, NULL, yuv_img_buffer, NULL);
   MVDSTD_SetConfig(&mvdstd_config);
 
   return init_px_to_framebuffer(surface_width, surface_height, surface_width, surface_height, pixel_size);
@@ -101,22 +113,20 @@ static int n3ds_init(int videoFormat, int width, int height, int redrawRate, voi
 static void n3ds_destroy(void) {
   y2rExit();
   mvdstdExit();
-  linearFree(n3ds_buffer);
-  linearFree(img_buffer);
+  linearFree(nal_unit_buffer);
+  linearFree(yuv_img_buffer);
+  linearFree(rgb_img_buffer);
   deinit_px_to_framebuffer();
 }
 
-static inline int write_yuv_to_framebuffer(u8 *dest, const u8 *source, int width, int height, int px_size) {
-  Handle conversion_finish_event_handle;
-  int status = 0;
-
-  status = Y2RU_SetSendingYUYV(source, width * height * 2, 800, 0);
+static inline int yuv_to_rgb(u8 *dest, const u8 *source, int width, int height, int px_size) {
+  int status = Y2RU_SetSendingYUYV(source, width * height * 2, 800, 0);
   if (status) {
     fprintf(stderr, "Y2RU_SetSendingYUYV failed\n");
     goto y2ru_failed;
   }
 
-  status = Y2RU_SetReceiving(img_buffer, width * height * px_size, 8, 0);
+  status = Y2RU_SetReceiving(dest, width * height * px_size, 8, 0);
   if (status) {
     fprintf(stderr, "Y2RU_SetReceiving failed\n");
     goto y2ru_failed;
@@ -133,37 +143,10 @@ static inline int write_yuv_to_framebuffer(u8 *dest, const u8 *source, int width
     fprintf(stderr, "Y2RU_GetTransferEndEvent failed\n");
     goto y2ru_failed;
   }
-
-  svcWaitSynchronization(conversion_finish_event_handle, 20000000);//Wait up to 20ms.
-  svcCloseHandle(conversion_finish_event_handle);
-  write_px_to_framebuffer(dest, img_buffer, px_size);
   return DR_OK;
 
   y2ru_failed:
   return -1;
-}
-
-static inline void mvd_frame_set_busy(unsigned char* framebuf) {
-  *framebuf = 0x11;
-  *(framebuf + (image_width * pixel_size - 1)) != 0x11;
-  *(framebuf + ((image_width * image_height * pixel_size) - (image_width * pixel_size))) != 0x11;
-  *(framebuf + (image_width * image_height * pixel_size - 1)) != 0x11;
-}
-
-static inline bool mvd_frame_ready(unsigned char* framebuf) {
-  if (mvdstdRenderVideoFrame(&mvdstd_config, false) != MVD_STATUS_BUSY) {
-    return true;
-  }
-
-  if(*framebuf != 0x11
-  || *(framebuf + (image_width * pixel_size - 1)) != 0x11
-  || *(framebuf + ((image_width * image_height * pixel_size) - (image_width * pixel_size))) != 0x11
-  || *(framebuf + (image_width * image_height * pixel_size - 1)) != 0x11)
-  {
-    return true;
-  }
-
-  return false;
 }
 
 // packets must be decoded in order
@@ -172,7 +155,7 @@ static inline int n3ds_decode(unsigned char* indata, int inlen) {
   int ret = mvdstdProcessVideoFrame(indata, inlen, 1, NULL);
   if(ret!=MVD_STATUS_PARAMSET && ret!=MVD_STATUS_INCOMPLETEPROCESSING)
   {
-    mvdstdRenderVideoFrame(&mvdstd_config, false);
+    mvdstdRenderVideoFrame(&mvdstd_config, true);
   }
   return 0;
 }
@@ -181,24 +164,28 @@ static int n3ds_submit_decode_unit(PDECODE_UNIT decodeUnit) {
   PLENTRY entry = decodeUnit->bufferList;
   int length = 0;
 
-  ensure_linear_buf_size(&n3ds_buffer, &n3ds_buffer_size, decodeUnit->fullLength + AV_INPUT_BUFFER_PADDING_SIZE);
+  ensure_linear_buf_size(&nal_unit_buffer, &nal_unit_buffer_size, decodeUnit->fullLength + AV_INPUT_BUFFER_PADDING_SIZE);
 
   while (entry != NULL) {
-    memcpy(n3ds_buffer+length, entry->data, entry->length);
+    memcpy(nal_unit_buffer+length, entry->data, entry->length);
     length += entry->length;
     entry = entry->next;
   }
-  GSPGPU_FlushDataCache(n3ds_buffer, length);
+  GSPGPU_FlushDataCache(nal_unit_buffer, length);
 
-  u8 *gfxtopadr = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
-  while (!mvd_frame_ready(img_buffer)) {
-    svcSleepThread(1000);
+  if (conversion_finish_event_handle != NULL) {
+    u8 *gfxtopadr = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
+
+    svcWaitSynchronization(conversion_finish_event_handle, 20000000);//Wait up to 20ms.
+    svcCloseHandle(conversion_finish_event_handle);
+
+    write_px_to_framebuffer(gfxtopadr, rgb_img_buffer, pixel_size);
+    gfxScreenSwapBuffers(GFX_TOP, false);
   }
-  write_yuv_to_framebuffer(gfxtopadr, img_buffer, surface_width, surface_height, pixel_size);
-  gfxScreenSwapBuffers(GFX_TOP, false);
 
-  mvd_frame_set_busy(img_buffer);
-  n3ds_decode(n3ds_buffer, length);
+
+  n3ds_decode(nal_unit_buffer, length);
+  yuv_to_rgb(rgb_img_buffer, yuv_img_buffer, surface_width, surface_height, pixel_size);
 
   // If MVD never gets an IDR frame, everything shows up gray
   if (first_frame) {
